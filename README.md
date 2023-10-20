06f57390157abeeceb5954d82ffe021074fdda94
gkaframanis <gkaframa@netcompany.com>
2023-10-20 15:45:50 +0300

    Fake authentication flow

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # WorldWise
 
-An app where you can keep track of the places you visited in the world.  
-The app was built using React, the json-server package was used to fake
-the API data, the react-leaflet library for the map and the context API
-and the reducer were practiced here to manage the state.
+- An app where you can keep track of the places you visited in the world.  
+  The app was built using React, the json-server package was used to fake
+  the API data, the react-leaflet library for the map and the context API
+  and the reducer were practiced here to manage the state.
+- Also implemented fake authentication flow.
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,31 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./Login.module.css";
 import { PageNav } from "../components/PageNav.jsx";
+import { useAuth } from "../contexts/FakeAuthContext.jsx";
+import Button from "../components/Button.jsx";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
   // PRE-FILL FOR DEV PURPOSES
   const [email, setEmail] = useState("jack@example.com");
   const [password, setPassword] = useState("qwerty");
+  const { login, isAuthenticated } = useAuth();
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (email && password) login(email, password);
+  };
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      // We overwrite the login page
+      navigate("/app", { replace: true });
+    }
+  }, [isAuthenticated, navigate]);
 
   return (
-    <main className={styles.login}>
+    <main className={styles.login} onSubmit={handleSubmit}>
       <PageNav />
       <form className={styles.form}>
         <div className={styles.row}>
@@ -32,7 +49,7 @@ export default function Login() {
         </div>
 
         <div>
-          <button>Login</button>
+          <Button type={"primary"}>Login</Button>
         </div>
       </form>
     </main>
